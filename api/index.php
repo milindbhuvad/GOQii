23d5b4dfbbad5e3b043bf9c3d7ad704c6caa3b4f
milindbhuvad <milindbhuvad1988@gmail.com>
2025-06-21 15:08:43 +0530

    Handling CORS preflight requests and enabling React app request to access backend.

--- api/index.php
+++ api/index.php
@@ -1,5 +1,12 @@
 <?php
-header("Content-Type: application/json");
+header("Access-Control-Allow-Origin: http://localhost:3000");
+header("Access-Control-Allow-Headers: Authorization, Content-Type");
+header("Access-Control-Allow-Methods: GET, POST, OPTIONS");
+
+if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
+    http_response_code(200);
+    exit();
+}
 
 $valid_user = 'admin';
 $valid_pass = 'Admin@123';
